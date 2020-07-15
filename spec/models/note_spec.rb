81a92f2606ed1b72a01c9d2ba05841de3a417b3c
Ji Shan <uug54332017@gmail.com>
2020-07-15 19:16:09 +1000

    note model test updated

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe Note, type: :model do
     it 'is valid with valid attritube' do 
       expect(subject).to be_valid
     end 
+
+    it { expect(subject).to validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_least(5).is_at_most(100) }
+    it { expect(subject).to validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(10) }
+    it { expect(subject).to validate_inclusion_of(:public_share).in_array([true,false])}
+    it { expect(subject).to validate_inclusion_of(:completed).in_array([true,false])}
   end
+
+  context "associations" do 
+    it { expect(subject).to have_many(:categories).through(:category_notes) }
+    it { expect(subject).to belong_to(:user)}
+    it { expect(subject).to have_many(:note_users) }
+  end
+
 end
