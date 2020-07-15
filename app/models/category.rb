00130e21172ab2d19999c69c71f22c63807e66a8
Ji Shan <uug54332017@gmail.com>
2020-07-15 19:47:19 +1000

    category & note-category & note-user test updated

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates_length_of :name , in: 5..35
+  
   has_many :category_notes
   has_many :notes, through: :category_notes
 end
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,5 +3,4 @@ class Cohort < ApplicationRecord
   has_many :user_cohorts
   has_many :users, through: :user_cohorts
 
- 
 end
