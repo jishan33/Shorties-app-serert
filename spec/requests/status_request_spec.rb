ee951b4a75b6ff259753aaa7efd685db9cd3e84b
Ji Shan <uug54332017@gmail.com>
2020-07-16 20:13:28 +1000

    status request tests completed

--- spec/requests/status_request_spec.rb
+++ spec/requests/status_request_spec.rb
@@ -1,5 +1,37 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Statuses", type: :request do
+  describe "GET #index" do
+    before(:example) do
+      user = create(:user)
+      @token = jwt_token(user)
+      get "/status", headers: authenticated_header(user)
 
+      @json_response = JSON.parse(response.body)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "JSON response contains the correct jwt" do
+      expect(@json_response["jwt"]).to eq(@token)
+    end
+  end
+
+  describe "GET #user" do
+    before(:example) do
+      @user = create(:user)
+      get "/status/user", headers: authenticated_header(@user)
+      @json_response = JSON.parse(response.body)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "JSON response contains the current user email" do
+      expect(@json_response["user"]).to eq(@user.email)
+    end
+  end
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,11 +1,14 @@
 module AuthenticationHelpers
-  def authenticated_header
-    user = create(:user)
-
-    token = Knock::AuthToken.new(payload: {sub: user.id}).token
+  def authenticated_header(user = create(:user))
+    token = jwt_token(user)
 
     { 'Authorization': "Bearer #{token}" }
   end
+
+  def jwt_token(user)
+    Knock::AuthToken.new(payload: {sub: user.id}).token
+  end  
+
 end
 
 RSpec.configure do |config|
