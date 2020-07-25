25ae2500bb69ec3b596708e834cbeeb16092b5fc
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-25 14:01:20 +1000

    cohort.users now returns students

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,7 +1,7 @@
 class Cohort < ApplicationRecord
-  belongs_to :user, -> { where is_teacher: true }
   has_many :user_cohorts
   has_many :users, through: :user_cohorts
+  belongs_to :user, -> { where is_teacher: true }
 
   validates :name, presence: true, uniqueness: true
 end
